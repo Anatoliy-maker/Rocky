f8153bdcae21b9b197b979ad7db8c54c326082a6
Anatoliy Savchenko <barholomewcubbins1995@gmail.com>
2025-03-31 10:45:01 +0400

    Created First Model

--- Rocky/obj/Debug/net9.0/Rocky.AssemblyInfo.cs
+++ Rocky/obj/Debug/net9.0/Rocky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rocky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd79a266d31ec6a666242824cc04c4abb6cd2992")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rocky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rocky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
