b57bfc521655a9b380a095f1a13c8767535c517b
Anatoliy Savchenko <barholomewcubbins1995@gmail.com>
2025-08-29 20:43:10 +0400

    Added a shopping cart to the app

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -141,7 +141,8 @@ namespace Rocky.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
-                    if (User.IsInRole(WC.AdminRole)) 
+
+                    if (User.IsInRole(WC.AdminRole))
                     {
                         //an admin has logged in and they try to create a new user
                         await _userManager.AddToRoleAsync(user, WC.AdminRole);
